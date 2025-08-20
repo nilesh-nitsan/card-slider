6c5a71b6be43e1a3528fb997b2630aaec448c538
Nilesh Malankiya <59409022+nilesh-nitsan@users.noreply.github.com>
2025-08-20 11:03:58 +0530

    Add ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,18 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Card Slider',
+    'description' => 'Create Slider extension that manage using Images',
+    'category' => 'plugin',
+    'author' => 'NITSAN',
+    'author_email' => '',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.0.0-13.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
